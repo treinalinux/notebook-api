f5007e5d55fcbdff342fca8f93cb404475ef3eef
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-27 06:04:42 -0300

    Default json_api on project

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,13 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthdate
+
+  def attributes(*args)
+    h = super(*args)
+    # Format date on pt-BR
+    # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    #
+    # Format date Default recommend of json_api use ISO 8601 W3C
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    h
+  end
 end
