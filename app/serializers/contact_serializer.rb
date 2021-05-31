473729866c66f42a79e2302ee83da6d2b23fd750
Alan da Silva Alves <linuxfama@gmail.com>
2021-05-31 05:52:28 -0300

    Working AMS Links (HATEOAS)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,17 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate #, :author
+  attributes :id, :name, :email, :birthdate # , :author
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind.id) }
+  end
+
   has_many :phones
   has_one :address
+
+  # link(:self) { contact_path(object) }
+  link(:self) { contact_url(object) }
+  # link(:kind) { kind_path(object.kind.id) }
+  link(:kind) { kind_url(object.kind.id) }
 
   # def author
   #   'Alan da Sila Alves'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  Rails.application.routes.default_url_options = {
+    host: 'localhost',
+    port: 3000
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
