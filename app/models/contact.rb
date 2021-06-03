26e6304f9ce17eb0846ef38673c226b8d705ed59
Alan da Silva Alves <linuxfama@gmail.com>
2021-06-03 07:43:41 -0300

    rubocop -A app/models/contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,9 +11,9 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
 
-  def as_json(options={})
+  def as_json(options = {})
     h = super(options)
-    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h[:birthdate] = (I18n.l(birthdate) unless birthdate.blank?)
     h
   end
   # def birthdate_br
